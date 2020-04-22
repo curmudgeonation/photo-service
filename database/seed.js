cc70c96977dd30edd04d0657af6c74b90f3e0fc2
clarissajaime <clarissanjaime@gmail.com>
2020-04-21 17:32:58 -0700

    update seed data to populate 100 entries

--- database/seed.js
+++ database/seed.js
@@ -1,44 +1,60 @@
 const db = require("./index.js");
 const Listing = require("./Listing.js");
 
-const listings = [
-  {
-    documentId: 1,
-    listingId: 1001,
-    assets: [
-      {
-        url:
-          "https://images.unsplash.com/photo-1556955112-28cde3817b0a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80",
-        description: "Lovely four bedroom, mid-century home",
-      },
-      {
-        url:
-          "https://images.unsplash.com/photo-1556912172-45b7abe8b7e1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
-        description: "Well-lit kitchen",
-      },
-      {
-        url:
-          "https://images.unsplash.com/photo-1509600110300-21b9d5fedeb7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
-        description: "Pool in the backyard",
-      },
-      {
-        url:
-          "https://images.unsplash.com/photo-1540518614846-7eded433c457?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1339&q=80",
-        description: "King-sized bed",
-      },
-      {
-        url:
-          "https://images.unsplash.com/photo-1574643156929-51fa098b0394?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
-        description: "Guest bedroom with bathroom",
-      },
-    ],
-  },
-];
+const LoremIpsum = require("lorem-ipsum").LoremIpsum;
 
-const insertSampleListings = function () {
-  Listing.create(listings).then(() => db.disconnect());
+const lorem = new LoremIpsum({
+  sentencesPerParagraph: {
+    max: 1,
+    min: 0,
+  },
+  wordsPerSentence: {
+    max: 6,
+    min: 3,
+  },
+});
+
+const insertSeedData = function () {
+  const seederData = [];
+
+  for (var i = 1; i < 101; i++) {
+    var singleProp = {
+      documentId: i,
+      listingId: 1000 + i,
+      assets: [
+        {
+          url: "https://source.unsplash.com/1600x900/?home",
+          description: lorem.generateWords(2),
+        },
+        {
+          url: "https://source.unsplash.com/1600x900/?home",
+          description: lorem.generateWords(2),
+        },
+        {
+          url: "https://source.unsplash.com/1600x900/?home",
+          description: lorem.generateWords(2),
+        },
+        {
+          url: "https://source.unsplash.com/1600x900/?home",
+          description: lorem.generateWords(2),
+        },
+        {
+          url: "https://source.unsplash.com/1600x900/?home",
+          description: lorem.generateWords(2),
+        },
+      ],
+    };
+
+    seederData.push(singleProp);
+  }
+
+  Listing.create(seederData)
+    .then(() => {
+      console.log("Success creating and seeding db.");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
-// To-do: Add a loop for 100 listings
-
-insertSampleListings();
+insertSeedData();
